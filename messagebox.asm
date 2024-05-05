81be11cbe4d0b391510451eda76b7e057a1c1c4b
totekuh <downtownxore@gmail.com>
2024-05-05 03:28:36 +0200

    add peutils

---
+++ messagebox.asm
@@ -0,0 +1,27 @@
+global _start
+extern _ExitProcess@4
+extern _MessageBoxA@16  ; Value after @ is size of args on stack
+section .text
+
+; Assembly with NASM for 32-bit
+; nasm -f win32 -o messagebox.o messagebox.asm
+
+; Linking for 32-bit explicitly with MinGW 32-bit linker
+; i686-w64-mingw32-ld -o messagebox.exe messagebox.o -luser32 -lkernel32
+
+_start:
+    ; MessageBoxA( NULL, str_message, "", 0 );
+        push 0
+        push str_empty
+        push str_message
+        push 0
+        call _MessageBoxA@16
+
+    ; ExitProcess( 0 );
+        push 0
+        call _ExitProcess@4
+
+section .data
+
+str_message  db  "hack the planet", 0
+str_empty    db  0
